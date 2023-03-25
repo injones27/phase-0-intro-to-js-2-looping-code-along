d553a57eab1665891c62ee49023c1c7ef2876890
Isaiah Jones <injones27@gmail.com>
2023-03-25 17:03:37 -0400

    Looping-Code-Along Lab Completed.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+let stringNames = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(stringNames) {
+    return [
+        `Thank you, ${stringNames[0]}, for the wonderful surprise gift!`,
+        `Thank you, ${stringNames[1]}, for the wonderful surprise gift!`,
+        `Thank you, ${stringNames[2]}, for the wonderful surprise gift!`,
+    ];
+};
+
+function countDown() {
+    for (let spy = 10; spy >= 0; spy--) {
+    console.log(spy)
+    };
+};
+
+for (let spy = 4; spy >= 0; spy--) {
+console.log(spy)
+}
